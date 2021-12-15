585c789552b53896720030191dad47ed64726da1
Rizwan Rahim <rizwrahim@gmail.com>
2021-12-15 12:15:12 +0400

    script

--- Mobile Cam.py
+++ Mobile Cam.py
@@ -1,7 +1,7 @@
 import cv2
-url = "http://192.168.1.5:8080/video"
+url = "http://ip address and port/video"
 cp = cv2.VideoCapture(url)
-while(True):
+while True:
     camera, frame = cp.read()
     if frame is not None:
         cv2.imshow("CAM", frame)
